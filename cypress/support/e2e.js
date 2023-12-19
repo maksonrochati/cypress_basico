1ae628cc80f3efd16eacd91a764054994c5c14b0
MaksonVia <109090857+MaksonVia@users.noreply.github.com>
2023-12-19 15:28:27 -0300

    update

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -5,29 +5,3 @@ import "@shelex/cypress-allure-plugin";
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 require("@shelex/cypress-allure-plugin");
-const app = window.top;
-if (!app.document.head.querySelector("[data-hide-command-log-request]")) {
-  const style = app.document.createElement("style");
-  style.innerHTML =
-    ".command-name-request, .command-name-xhr { display: none };";
-  style.setAttribute("data-hide-command-log-request", "");
-  app.document.head.appendChild(style);
-}
-export {
-  Scenario,
-  Given,
-  When,
-  And,
-  Then,
-  Cenario,
-  Dado,
-  Quando,
-  E,
-  Entao,
-} from "cypress-action/src/gherkin/bdd.js";
-import "cypress-plugin-steps";
-require("cypress-action");
-import "cypress-file-upload";
-require("cypress-xpath");
-export const faker = require("generate-datafaker");
-import "cypress-wait-until";
